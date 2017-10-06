5b7636f73493907209ab02eac448b6e90c17b268
aslanahmet <ahmet@kodeveloper.co>
2017-10-06 14:32:32 +0300

    test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 
 driver = webdriver.Remote(
-            command_executor='http://localhost:5550/wd/hub',
+            command_executor='http://172.31.36.129:4444/grid/register/',
             desired_capabilities={'browserName': 'chrome'})
 driver.get('https:www.google.com')
 driver.quit()
